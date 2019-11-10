d8631107b070882aa3cb016d1a82deb92787bd6e
Stark <stark@pop-os.localdomain>
2019-11-10 12:33:16 +0100

    Updates

---
+++ precache-manifest.69269b88ba3c9c74faa42b38b38877dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc36348d1d4a03f4c5fb67cd825ec2f",
+    "url": "/my-monsters/index.html"
+  },
+  {
+    "revision": "d72a554d5dbc8fa90dd4",
+    "url": "/my-monsters/static/css/main.734a976d.chunk.css"
+  },
+  {
+    "revision": "77348947c25dd6a911c4",
+    "url": "/my-monsters/static/js/2.296ca05d.chunk.js"
+  },
+  {
+    "revision": "d72a554d5dbc8fa90dd4",
+    "url": "/my-monsters/static/js/main.454a00fd.chunk.js"
+  },
+  {
+    "revision": "e04876792c9b649c7f44",
+    "url": "/my-monsters/static/js/runtime-main.cc8dfe36.js"
+  }
+]);
